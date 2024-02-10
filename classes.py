2d0a4b75ab5772248fe029f6f9c73bee69d1f403
nicoa47 <nicoadelh47@gmail.com>
2024-02-10 21:07:33 +0100

    fixed most collision issues

--- classes.py
+++ classes.py
@@ -3,6 +3,11 @@ from utility_functions import *
 
 # TODO collision detection
 
+class World:
+    def __init__(self, gravity = 0.5, friction = 0.9):
+        self.gravity = gravity # acceleration in y dir
+        self.friction = friction # percentage of velocity kept between frames
+
 class CircleObject:
     def __init__(self, window, start_x, start_y, radius):
         self.window = window
@@ -19,19 +24,54 @@ class CircleObject:
         pygame.draw.circle(self.window, self.color, (self.x, self.y), self.radius, width=0)
 
 class PlayerCircle(CircleObject):
-    def __init__(self, window, start_x, start_y, radius, gravity, gravity_cap):
+    def __init__(self, window, start_x, start_y, radius, world, max_vel):
         self.window = window
         self.x = start_x
         self.y = start_y
         self.vel_x = 0
         self.vel_y = 0
         self.acc_x = 0
-        self.acc_y = gravity # note: did not consider it necessary to make it a class var since gravity stays
+        self.acc_y = world.gravity # note: did not consider it necessary to make it a class var since gravity stays
         self.radius = radius
         self.color = "white"
-        self.gravity_cap = gravity_cap
+        self.world = world
+        self.max_vel = max_vel
+        self.found_overlap = False
+        self.found_ground = False
+        self.no_vel = True
 
-    def check_for_collisions(self, others):
+    # def ground_underneath(self):
+    #     # bool function
+    #     # given current position, check one ??? down from the underside of the player circle
+    #     current_feet_y = self.y + self.radius
+    #     pass # TODO remove if not needed anymore
+
+    def ground_underneath(self, others):
+        # bool function
+        virt_end_x = self.x
+        virt_end_y = self.y + self.radius
+
+        # only check a small portion of the underside of the player, since only the feet area should be tested
+        check_radius = self.radius*0.05
+
+        self.found_ground = False
+        for other in others:
+            # assuming that others are circles for now
+            if overlaps(virt_end_x, virt_end_y, check_radius, other.x, other.y, other.radius):
+                self.color = "red"
+                self.found_ground = True
+                return True
+        
+        self.color = "white"
+        return False
+
+    def correct_for_collisions(self, others):
+
+        # TODO problems/observations:
+        # - immersing quite deeply into obstacle at first before correcting - esp. visible when pressing keys
+        # - (seems solved) no collision detected when constant acceleration
+        # - (seems solved) velocity after adjustment is very difficult to predict intuitively, definitely not the same velocity as before 
+
         # depending on velocity vector: run intermediate checks
         # via linear interpolation
         virt_end_x = self.x + self.vel_x + self.acc_x # TODO check if this does not change the original value!!
@@ -43,35 +83,64 @@ class PlayerCircle(CircleObject):
         virt_len = vec_len(self.x, self.y, virt_end_x, virt_end_y)
 
         # divide the vec by steps depending on player velocity
-        n_steps = int(vec_len(0, 0, self.vel_x, self.vel_y)/2)
+        n_steps = 60 #int(vec_len(0, 0, self.vel_x, self.vel_y)/2) # TODO make a reasonable number, also based on acceleration!
         step_len = virt_len/n_steps if n_steps > 0 else 1 # no velocity in beginning means 0 steps: correct
 
-        virt_vec_norm_x = virt_vec_x/virt_len
-        virt_vec_norm_y = virt_vec_y/virt_len
+        if virt_len > 0:
+            self.no_vel = False
+            virt_vec_norm_x = virt_vec_x/virt_len
+            virt_vec_norm_y = virt_vec_y/virt_len
 
-        for n_step in range(n_steps):
-            test_x = self.x + virt_vec_norm_x*(n_step*step_len)
-            test_y = self.y + virt_vec_norm_y*(n_step*step_len)
+            self.found_overlap = False
 
-            # TODO not clear what happens when a correction is done - need to adjust virtually first?
+            for i, other in enumerate(others):
 
-            for other in others:
-                # assuming that others are circles for now
-                if overlaps(test_x, test_y, self.radius, other.x, other.y, other.radius):
-                    vec_x, vec_y = overlap_correction_vector(test_x, test_y, self.radius, other.x, other.y, other.radius)
-                    self.update_pos(vec_x, vec_y)
+                for n_step in range(n_steps):
+                    test_x = self.x + virt_vec_norm_x*(n_step*step_len)
+                    test_y = self.y + virt_vec_norm_y*(n_step*step_len)
+
+                    # TODO not clear what happens when a correction is done - need to adjust virtually first?
+
+                    # assuming that others are circles for now
+                    if overlaps(test_x, test_y, self.radius, other.x, other.y, other.radius):
+                        vec_x, vec_y = overlap_correction_vector(test_x, test_y, self.radius, other.x, other.y, other.radius)
+                        self.update_pos(vec_x, vec_y)
+                        self.found_overlap = True
+                        break # this guy hopefully prevents weird bouncy behavior
+                        # # TODO very experimental!
+                        # self.vel_x, self.vel_y = 0, 0
+        else:
+            self.no_vel = True
+
 
     def update_pos(self, x, y):
-        self.x += x
-        self.y += y
+        # adds to the velocity, but velocity is kept within bounds of self.max_vel 
+        # (see self.update() function)
+        # maybe only allow this direct setting in a certain mode
+        self.vel_x += x
+        self.vel_y += y
 
     def update(self):
+
+        # friction only applied in orthogonal directions --> slight disregard of diagonal dirs
+        # but accept for now as a compromise for computation
+        # don't want to apply friction to acc (such as gravity) - so applying to vel before acc
+        self.vel_x *= self.world.friction
+        self.vel_y *= self.world.friction
+
         self.vel_x += self.acc_x
         self.vel_y += self.acc_y
 
-        # apply gravity cap
-        if self.vel_y > self.gravity_cap: self.vel_y = self.gravity_cap
-
+        # cap the velocity
+        # check the current speed
+        vel = dist([0, 0], [self.vel_x, self.vel_y])
+        if vel > self.max_vel:
+            # adjust velocity
+            self.vel_x = (self.vel_x/vel)*self.max_vel
+            self.vel_y = (self.vel_y/vel)*self.max_vel
+        # new_vel = dist([0, 0], [self.vel_x, self.vel_y])
+        # print(self.max_vel, vel, new_vel)
+            
         self.x += self.vel_x
         self.y += self.vel_y
 
@@ -82,6 +151,7 @@ class PlayerCircle(CircleObject):
         elif self.y > self.radius + self.window.get_height(): self.y = -self.radius
 
 class Platform:
+
     # for now just a line
     # for now only stationary: tuples hold the coords
     def __init__(self, window, start_x, start_y, stop_x, stop_y):
@@ -90,4 +160,16 @@ class Platform:
         self.pos_stop = (stop_x, stop_y)
         
     def draw(self):
+
         pygame.draw.line(self.window, "white", self.pos_start, self.pos_stop, width=1)
+
+class DebugText:
+
+    def __init__(self, window, text):
+        self.window = window
+        self.text = text
+        self.font = pygame.font.SysFont(None, 48)
+
+    def draw(self):
+        pygame_text = self.font.render(self.text, True, (255, 255, 255))
+        self.window.blit(pygame_text, (50, 50))
--- screen_test.py
+++ screen_test.py
@@ -16,28 +16,42 @@ window = pygame.display.set_mode(window_size)
 # Set up the clock for managing the frame rate
 clock = pygame.time.Clock()
 
+world = World()
 obstacles = []
 key_vel = 4
-gravity = 0.07
-gravity_cap = 6.5
+max_vel = 10
 
 # instantiating world objects
 player = PlayerCircle(
-    window, screen_w/2, screen_h/2, 20, gravity, gravity_cap
+    window, screen_w/2, screen_h/2, 20, world, max_vel
     )
-for obst_ind in range(20):
+for obst_ind in range(10):
     randx = random.random()
     randy = random.random()
     randr = random.random()
     obstacle = CircleObject(
-        window, screen_w*randx, screen_h*randy, 80*randr
+        window, screen_w*randx, screen_h*randy, 5+80*randr
     )
     obstacles.append(obstacle)
-platform = Platform(
-    window,
-    50, screen_h-100, 
-    screen_w-50, screen_h-100
-    )
+
+# lower obstacle
+# obstacle = CircleObject(
+#     window, screen_w/2, screen_h*0.75, 40
+# )
+# obstacles.append(obstacle)
+
+# upper obstacle
+# obstacle = CircleObject(
+#     window, screen_w/2, screen_h*0.25, 40
+# )
+# obstacles.append(obstacle)
+
+# platform = Platform(
+#     window,
+#     50, screen_h-100, 
+#     screen_w-50, screen_h-100
+#     )
+debugtext = DebugText(window, "TEST")
 
 # Main game loop
 running = True
@@ -45,41 +59,75 @@ fps_display_interval = 1  # seconds
 last_fps_display_time = 0
 
 while running:
-    current_time = pygame.time.get_ticks()  # Get current time in milliseconds
-    # Event handling loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            # Check for specific key presses
-            if event.key == pygame.K_ESCAPE:
-                running = False
 
-    # continuous key presses
-    keys = pygame.key.get_pressed()
-    for vel, key in zip(
-        [(0, -key_vel), (0, key_vel), (-key_vel, 0), (key_vel, 0)], 
-        [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
-        ):
-        if keys[key]: player.update_pos(*vel)
-    if keys[pygame.K_DOWN]:  # Check if the spacebar is being held down
-        player.update_pos(0, -0.2)
-    
-    # Update game state here
-    player.check_for_collisions(obstacles)
-    player.update()
-    
-    # Draw everything here
-    window.fill((0, 0, 0))  # Fill the window with black
-    player.draw()
-    platform.draw()
-    for obstacle in obstacles: obstacle.draw()
-    
-    # Update the display
-    pygame.display.flip()
-    
-    # Cap the frame rate to 60 frames per second
-    clock.tick(60)
+    # go_to_next_frame = False
+
+    # # Button press event
+    # if not go_to_next_frame:
+    #     # Perform action based on the key pressed
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+    #         if event.type == pygame.KEYDOWN:
+    #             if event.key == pygame.K_SPACE:  # For example, waiting for the space bar
+    #                 go_to_next_frame = True
+
+    # if go_to_next_frame:
+
+        current_time = pygame.time.get_ticks()  # Get current time in milliseconds
+        # Event handling loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                # Check for specific key presses
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+        # continuous key presses
+        keys = pygame.key.get_pressed()
+        for vel, key in zip(
+            [(0, -key_vel), (0, key_vel), (-key_vel, 0), (key_vel, 0)], 
+            [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
+            ):
+            if keys[key]: player.update_pos(*vel)
+        
+        # Update game state here
+        # if player.ground_underneath(obstacles): 
+        #     # player.acc_y = 0 # gravity "not active"
+        #     # player.vel_y = 0 # make player stop immediately (accumulated gravity is stopped)
+        #     pass
+        # else: 
+        #     player.acc_y = gravity
+            
+        # importantly, collisions need to be checked before the update,
+        # otherwise keypresses will allow player to move through obstacles
+        player.correct_for_collisions(obstacles)
+        player.update()
+        if player.no_vel: debugtext.text = "NO VELOCITY"
+        else: debugtext.text = ""
+        
+        # Draw everything here
+        window.fill((0, 0, 0))  # Fill the window with black
+        player.draw()
+        debugtext.draw()
+        # platform.draw()
+        for obstacle in obstacles: obstacle.draw()
+        
+        # Update the display
+        pygame.display.flip()
+        
+        # Cap the frame rate to n frames per second
+        clock.tick(30)
+
+        # while not go_to_next_frame:
+
+        #     if event.type == pygame.KEYDOWN:
+        #         # Check if the key pressed is the space bar
+        #         if event.key == pygame.K_SPACE:
+        #             go_to_next_frame = True
+
+        #     clock.tick(30)
 
 # Clean up Pygame and exit
 pygame.quit()
